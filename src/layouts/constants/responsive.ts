39e07fa8ea2829f42534d9fffa59d1b60d6a81a1
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-12-03 01:34:50 +0900

    ✨ feat: 반응형 스타일 적용을 위한 mediaQuery Emotion 객체 구현

--- src/layouts/constants/responsive.ts
+++ src/layouts/constants/responsive.ts
@@ -1,7 +1,15 @@
-export type Breakpoint = Record<'sm' | 'md' | 'lg' | 'xl', number>;
-export const breakpoints: Breakpoint = {
+type Breakpoint = Record<'sm' | 'md' | 'lg', number>;
+const breakpoints: Breakpoint = {
   sm: 0,
   md: 600,
-  lg: 900,
-  xl: 1200
+  lg: 900
+};
+const breakpoint: Breakpoint = {
+  ...breakpoints
+};
+
+export const mediaQuery: Record<keyof Breakpoint, string> = {
+  sm: `@media (max-width: ${breakpoint.md}px)`,
+  md: `@media (max-width: ${breakpoint.md}px)`,
+  lg: `@media (max-width: ${breakpoint.lg}px)`
 };
