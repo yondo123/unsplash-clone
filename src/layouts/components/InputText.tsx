6fbcf357d89c4bf7a10706fb3cf90b9f4d88b905
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-12-02 19:04:50 +0900

    ✨ feat: InputText UI 컴포넌트 구현

--- src/layouts/components/InputText.tsx
+++ src/layouts/components/InputText.tsx
@@ -2,20 +2,27 @@
 
 import { useId } from 'react';
 import { Hidden } from './Hidden';
-import { InputTextBase } from '../styles/inputTextStyle';
+import { Button } from './Button';
+import { InputTextBase, InputTextWrapper } from '../styles/inputTextStyle';
 
 interface InputTextProps extends React.HTMLAttributes<HTMLInputElement> {
   labelText?: string;
+  submitIcon?: React.ReactNode;
 }
 
-export const InputText = ({ id, labelText, ...restProps }: InputTextProps) => {
+export const InputText = ({ id, labelText, submitIcon, ...restProps }: InputTextProps) => {
   const tempId = useId();
   const textId = id ?? `text-input-${id}`;
 
   return (
-    <>
+    <InputTextWrapper>
       <Hidden>{labelText ?? 'text input'}</Hidden>
       <InputTextBase type="text" id={textId} aria-labelledby={tempId} {...restProps} />
-    </>
+      {submitIcon && (
+        <Button variant="ghost" type="submit">
+          {submitIcon}
+        </Button>
+      )}
+    </InputTextWrapper>
   );
 };
--- src/layouts/styles/inputTextStyle.ts
+++ src/layouts/styles/inputTextStyle.ts
@@ -1,13 +1,22 @@
 import styled from '@emotion/styled';
+import { mediaQuery } from '../constants/responsive';
 
 export const InputTextWrapper = styled.div`
-  margin: 0 auto;
-  padding: 4px 10px;
+  position: relative;
+  width: 720px;
+  > button {
+    position: absolute;
+    top: 75%;
+    right: 10px;
+    transform: translateY(-75%);
+  }
+  ${mediaQuery.lg} {
+    width: 100%;
+  }
 `;
 
 export const InputTextBase = styled.input`
   width: 100%;
-  max-width: 720px;
   margin: 0;
   padding: 16px 12px;
   font-weight: 700;
