e2c1ef364b0cc8de92a34cdf1b47b9a82d375f3b
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-12-03 01:56:19 +0900

    ✨ feat: 검색 결과 입력 후 페이지 파라미터로 라우팅 처리

--- src/photo/components/PhotoHero.tsx
+++ src/photo/components/PhotoHero.tsx
@@ -1,8 +1,11 @@
 'use client';
 
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import { Text, Heading, InputText } from '@layouts/components';
 import { useTheme } from '@emotion/react';
+import { Search } from '@layouts/icons';
+import { useState } from 'react';
 import { PhotoHeroContainer, SearchInputWrapper } from '../styles/photoheroStyle';
 import type { Photo } from '@photo/types';
 
@@ -11,22 +14,39 @@ interface PhotoHeroProps {
 }
 
 export const PhotoHero = ({ photo }: PhotoHeroProps) => {
+  const router = useRouter();
+  const [searchKeyword, setSearchKeyword] = useState<string>('');
   const defaultTheme = useTheme();
   const fontColor = defaultTheme.colors.white;
+
   return (
     <PhotoHeroContainer>
       <Image alt={photo?.alt_description} layout="fill" objectFit="cover" src={photo?.urls.full} />
       <SearchInputWrapper>
-        <Heading size="xxl" color={fontColor}>
-          Will Photo
-        </Heading>
-        <Text size="lg" color={defaultTheme.colors.accent} css={{ marginTop: '16px' }}>
-          인터넷의 시각자료 출처입니다.
-        </Text>
-        <Text size="lg" color={defaultTheme.colors.accent}>
-          모든 지역에 있는 크리에이터들의 자원을 받습니다.
-        </Text>
-        <InputText placeholder="고해상도 이미지 검색." css={{ marginTop: '16px' }} />
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            router.push(`/?query=${searchKeyword}`);
+          }}
+        >
+          <Heading size="xxl" color={fontColor}>
+            Will Photo
+          </Heading>
+          <Text size="lg" color={defaultTheme.colors.accent} css={{ marginTop: '16px' }}>
+            인터넷의 시각자료 출처입니다.
+          </Text>
+          <Text size="lg" color={defaultTheme.colors.accent}>
+            모든 지역에 있는 크리에이터들의 자원을 받습니다.
+          </Text>
+          <InputText
+            onChange={e => {
+              setSearchKeyword(e.currentTarget.value);
+            }}
+            submitIcon={<Search />}
+            placeholder="고해상도 이미지 검색."
+            css={{ marginTop: '16px' }}
+          />
+        </form>
       </SearchInputWrapper>
     </PhotoHeroContainer>
   );
