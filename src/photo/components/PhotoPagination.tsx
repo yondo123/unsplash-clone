98f63201c010ca107c6908c74ca7b1c73f3f0126
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-12-04 04:16:22 +0900

    🔧 modify: 페이지 이동시 router 함수 호출 방식으로 변경

--- src/photo/components/PhotoPagination.tsx
+++ src/photo/components/PhotoPagination.tsx
@@ -1,12 +1,10 @@
 'use client';
 
-import Link from 'next/link';
 import { useSearchParams, useRouter } from 'next/navigation';
 import { PaginationContainer, PaginationList, PaginationItem } from '@photo/styles/photoPaginationStyle';
-import { Button, Text } from '@layouts/components';
+import { Button } from '@layouts/components';
 import { ArrowLeft, ArrowRight } from '@layouts/icons';
 import { createRange } from '@shared/utils/array';
-import { useTheme } from '@emotion/react';
 import { calculatePagination } from '../utils/calculatePagination';
 
 interface PhotoPaginationProps {
@@ -16,12 +14,15 @@ interface PhotoPaginationProps {
 
 export const PhotoPagination = ({ totalPages, pageSize }: PhotoPaginationProps) => {
   const FIRST_PAGE = 1;
-  const { colors } = useTheme();
   const router = useRouter();
   const params = useSearchParams();
   const currentPage = params.get('page') ? Number(params.get('page')) : FIRST_PAGE;
   const { startPage, endPage } = calculatePagination({ currentPage, totalPages, pageSize });
   const pageNumbers = createRange(startPage, endPage);
+
+  const movePage = (page: number) => {
+    router.push(`/?page=${page}`);
+  };
 
   const movePrevPage = () => {
     const prevPage = currentPage - 1;
@@ -35,11 +36,15 @@ export const PhotoPagination = ({ totalPages, pageSize }: PhotoPaginationProps) 
 
   const pageList = pageNumbers.map(page => (
     <PaginationItem key={page}>
-      <Link href={`/?page=${page}`}>
-        <Text size="xl" color={page === currentPage ? colors.black : colors.font}>
-          {page}
-        </Text>
-      </Link>
+      <Button
+        variant="ghost"
+        disabled={page === currentPage}
+        onClick={() => {
+          movePage(page);
+        }}
+      >
+        {page}
+      </Button>
     </PaginationItem>
   ));
 
