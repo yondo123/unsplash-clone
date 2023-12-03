8ceda6c89cb2d2dd6fd96db31a0b710e65c44f62
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-12-04 04:26:00 +0900

    🔧 modify: 페이지 파라미터 타입 변경

--- src/app/types.ts
+++ src/app/types.ts
@@ -1,2 +1,2 @@
 export type PageParams = { slug: string[] };
-export type PageSearchParams = { [key: string]: string | string[] | undefined };
+export type PageSearchParams = { [key: string]: string | undefined };
